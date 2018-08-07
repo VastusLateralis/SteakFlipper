e593f7c667e8bc21de465803c911732a411d7aa0
Thomas Johannessen <thomasjohannessen@thomass-macbook-pro.local>
2018-08-07 23:15:31 +0200

    Initial Commit

---
+++ SteakFlipper/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SteakFlipper
+//
+//  Created by Thomas Johannessen on 07/08/2018.
+//  Copyright © 2018 Thomas Johannessen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
