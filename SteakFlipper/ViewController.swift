87c88491fa58cc55b9f67b6ffd5cfdd3d85b2426
Thomas Johannessen <thomasjohannessen@thomass-macbook-pro.local>
2018-08-08 00:07:25 +0200

    First push

--- SteakFlipper/ViewController.swift
+++ SteakFlipper/ViewController.swift
@@ -8,13 +8,57 @@
 
 import UIKit
 
+//MARK: App Init
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    
+//MARK: Variables
+    @IBOutlet weak var biff_Steketid: UILabel!
+    @IBOutlet weak var biff_Flips: UILabel!
+    @IBOutlet weak var biff_StatusVindu: UILabel!
+    
+    var second = 60
+    var timer = Timer()
+    var timerRunning = true
+    
+    
+//MARK: Functions
+    
+    func setTimer(){
+        second  -= 1
+        biff_StatusVindu.text = "\(second)"
+        if second == 0 {
+            timerRunning = false
+            timer.invalidate()
+        }
+    }
+    
+    
+    func timerCounting(){
+        if(timerRunning == true){
+            timer = Timer.scheduledTimerWithTimeInterval(1, target: self, selector: Selector("setTimer"), userInfo: nil, repeats: true)
+            timerRunning = true
+            if second == 0 {
+                timerRunning = false
+                timer.invalidate()
+            }
+        }
+    }
+    
+    
+//MARK: Code
 
+    
+    
+    
+    
+    
+//MARK: App Exit
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
